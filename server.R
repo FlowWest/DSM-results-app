f5db53e30401826884b41510c6eb093abfec8f5b
alvizek <avizek@flowwest.com>
2021-09-08 11:56:43 -0700

    Makes checkbox functional and title reactive

--- server.R
+++ server.R
@@ -1,21 +1,37 @@
 server <- function(input, output) {
-  # Filter based on run and 
+  # Filter based on run and metric
   selected_results <- reactive({
-    dsm_results %>% filter(run == input$Run, 
-                           metric == input$Metric)
+      dsm_results %>% 
+        filter(run == input$Run, 
+               metric == input$Metric)
   })
   
   output$results_plot <- renderPlotly({
-    selected_results() %>% 
-      plot_ly(y = ~strategy, x = ~value, type = "bar",
-              orientation = 'h', 
-              hoverinfo = 'text') %>% 
-      layout(title = "Metric", 
-             yaxis = list(title = list(text ='')),
+    p_template <- function (data) {
+      data %>%
+        plot_ly(y = ~strategy, x = ~value, type = "bar",
+                          orientation = 'h', 
+                          hoverinfo = 'text') %>% 
+      layout(yaxis = list(title = list(text ='')),
              xaxis = list(title = list(text ='Utility Score')),
              barmode = 'stack',
              legend = list(orientation = 'h')) %>% 
       config(displayModeBar = FALSE)
+    }
+    
+    plot_2019 <- p_template(selected_results() %>%
+      filter(version == 2019))
+    plot_2021 <- p_template(selected_results() %>%
+      filter(version == 2021))
+    
+    if (input$results_2019 == T) {
+      subplot(plot_2019, plot_2021) %>%
+        layout(title = input$Metric)
+      
+    } else {
+      plot_2021 %>%
+        layout(title = input$Metric)
+    }
   })
   
   # output$text <- renderUI({
